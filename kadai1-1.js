04964d34014adb108b43e8304d11c5a150ca7f07
yukinobu fujii <y.f.mynovember0818@gmail.comgit config --global user.email y.f.mynovember0818@gmail.com>
2022-02-26 11:39:25 +0900

    課題完了ソフトコーディング

---
+++ kadai1-1.js
@@ -0,0 +1,12 @@
+let Fizz = 3;
+let Buzz = 5;
+let FizzBuzz = Fizz * Buzz;
+for (let i = 1; i < 100; i++){
+  if (i % FizzBuzz === 0) {
+    console.log(`FizzBuzz ${i}`);
+  } else if (i % Buzz === 0) {
+    console.log(`Buzz ${i}`);
+  } else if (i % Fizz === 0){
+    console.log(`Fizz ${i}`);
+  }
+}
